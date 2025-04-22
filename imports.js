2fcd1ac19eb90fa04fe986c7e583b3573110c0f3
lorenasandoval88 <lorenasandoval88@users.noreply.github.com>
2025-04-22 16:00:21 -0400

    rename imports to d3 etc

--- imports.js
+++ imports.js
@@ -3,7 +3,6 @@ const dataset = (await import("https://esm.sh/ml-dataset-iris"))
 import { default as PCAjs } from 'https://cdn.jsdelivr.net/npm/pca-js@1.0.1/+esm'
 const Plotly = (await import('https://cdn.jsdelivr.net/npm/plotly.js-dist@3.0.1/+esm')).default
 const localForage = (await import('https://cdn.jsdelivr.net/npm/localforage@1.10.0/dist/localforage.min.js'))
- 
 // import {PCA} from 'https://cdn.jsdelivr.net/npm/ml-pca@4.1.1/+esm'
 import * as d3 from "https://cdn.skypack.dev/d3@7"
 import { default as d3tip} from 'https://esm.sh/d3-tip';
@@ -12,15 +11,15 @@ import { default as d3tip} from 'https://esm.sh/d3-tip';
 // console.log("Plotly", Plotly)
 // console.log("PCA", PCA)
 
-const imports = {}
+// const imports = {}
 
-imports.divCount = 0
-imports.d3 = d3
-imports.d3tip = d3tip
-imports.Plotly = Plotly
-imports.PCA = PCA
-imports.PCAjs = PCAjs
-imports.iris = dataset
-imports.localForage = localForage
+// imports.d3 = d3
+// imports.d3tip = d3tip
+// imports.Plotly = Plotly
+// imports.PCA = PCA
+// imports.PCAjs = PCAjs
+// imports.iris = dataset
+// imports.localForage = localForage
 
-export { imports };
+// export { imports };
+export { PCA, PCAjs, Plotly, d3, d3tip, dataset, localForage };
