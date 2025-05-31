3eefe370595eeef214680b13224d5dbe588ee157
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2025-05-31 01:51:08 -0400

    added iris dataset from root folder

--- imports.js
+++ imports.js
@@ -1,30 +1,18 @@
+console.log("imports.js loaded")
+
 import { PCA as npm_pca } from "https://esm.sh/ml-pca"
 const ml_dataset_iris = (await import("https://esm.sh/ml-dataset-iris"))
 import { default as npm_pcajs } from 'https://cdn.jsdelivr.net/npm/pca-js@1.0.1/+esm'
 const Plotly = (await import('https://cdn.jsdelivr.net/npm/plotly.js-dist@3.0.1/+esm')).default
-const localForage = (await import('https://cdn.jsdelivr.net/npm/localforage@1.10.0/dist/localforage.min.js'))
+const localForage = (await import('https://cdn.jsdelivr.net/npm/localforage@1.10.0/+esm')).default
 // import {PCA} from 'https://cdn.jsdelivr.net/npm/ml-pca@4.1.1/+esm'
 import * as d3 from "https://cdn.skypack.dev/d3@7"
 import { default as d3tip} from 'https://esm.sh/d3-tip';
 const hclust = (await import("https://cdn.skypack.dev/ml-hclust@3.1.0?min"))
 // const dist = (await import("https://bundle.run/ml-distance-matrix@2.0.1"))
 import {default as dist} from "https://esm.sh/ml-distance-matrix"
-
 const distance = (await import("https://cdn.skypack.dev/ml-distance@3.0.0?min")).distance
-// console.log("Plotly", Plotly)
-// console.log("PCA", PCA)
 
-// const imports = {}
-
-// imports.d3 = d3
-// imports.d3tip = d3tip
-// imports.Plotly = Plotly
-// imports.PCA = PCA
-// imports.PCAjs = PCAjs
-// imports.iris = dataset
-// imports.localForage = localForage
-
-// export { imports };
 
 
 export { npm_pca, npm_pcajs, Plotly, d3, d3tip, ml_dataset_iris, localForage,hclust, dist, distance };
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import {pca_UI} from './modules/myPca.mjs'
 import {hclust_plot} from './modules/myDendogram.mjs'
 import {heatmap_plot} from './modules/myHeatmap.mjs'
+import {csvToJson} from '../otherFunctions.js'
 
-
-
-
+// retreive built-in iris dataset
+fetch('/sampleData/iris.csv')
+.then(response => response.text())
+.then(async data => console.log(await csvToJson(data)));
 
 pca_UI({divid:"myPCA"})
 
